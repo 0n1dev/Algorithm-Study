55631cf55b8f794127b3c6106fe7dfe9954bf30f
0n1dev <all_mind@naver.com>
2021-08-23 23:05:06 +0900

    feat : 스트림 없이 풀이

--- Programmers/위장 직접 풀어보기.java
+++ Programmers/위장 직접 풀어보기.java
@@ -13,13 +13,11 @@ class Solution {
                         });
 
         Integer value = Arrays.stream(maps.values().toArray(new Integer[0]))
-                .reduce(1, (e1, e2) -> e1 * e2);
+                .reduce(1, (e1, e2) -> e1 * (e2 + 1));
+        
+        
 
-        if (maps.size() == 1) {
-            value = 0;
-        }
-
-        return answer + value;
+        return value - 1;
     }
 
     public static void main(String[] args) {
@@ -31,3 +29,25 @@ class Solution {
 
     }
 }
+
+---
+
+// 스트림 없이
+
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+        for (String[] c : clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        }
+
+        Set<String> keys = map.keySet();
+
+        for (String key : keys) {
+            answer *= (map.get(key) + 1);
+        }
+
+        return answer - 1;
+    }
